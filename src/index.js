e4dfb3f2e6b4912a3b6a85d4834acd6fad5838a2
theoddorrrrr <aidar140702@mail.ru>
2022-08-07 21:03:28 +0600

    added loader

--- src/index.js
+++ src/index.js
@@ -8,5 +8,7 @@ if (
   window.location.pathname === "/" ||
   window.location.pathname === "/index.html"
 ) {
-    
+    setTimeout(() => {
+      document.querySelector(".loading").classList.add("hide")
+    }, 2000)
 }
